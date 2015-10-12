07db2eeb2b9ebbd1c087a69d0fd05a6929d4f79a
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-10-11 20:58:08 -0700

    Added main function to jackpot

--- jackbot/jackbot.py
+++ jackbot/jackbot.py
@@ -1,4 +1,6 @@
 import csv
+import os
+import sys
 
 # Open pornhub iframes
 #with open('pornhub.com-db.csv', newline='') as porns:
@@ -7,3 +9,14 @@ import csv
 #        print(row + "\n")
 
 
+def main():
+
+    # Can't do much without the list of iframes
+    if not os.path.isfile('pornhub.com-db.csv'):
+        print("Need to download the csv of pornhub iframes: ")
+        print("http://pornhub.com/pornhub.com-db.zip")
+        sys.exit(0);
+   
+    
+if __name__ == '__main__':
+    main()
