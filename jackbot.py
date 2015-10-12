a0bbb13cf6145fa3527b4e94cbd4fd460840c0f1
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-10-11 20:21:41 -0700

    Initial commit

---
+++ jackbot.py
@@ -0,0 +1,9 @@
+import csv
+
+# Open pornhub iframes
+#with open('pornhub.com-db.csv', newline='') as porns:
+#    pornreader = csv.reader(porns, delimiter=',', quotechar='|')
+#    for row in pornreader:
+#        print(row + "\n")
+
+
